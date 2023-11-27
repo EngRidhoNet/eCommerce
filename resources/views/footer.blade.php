aa76b5d02a3905ce697d0181761995054ee4beac
Ridho Aulia Rahman <220605110044@student.uin-malang.ac.id>
2023-11-27 13:24:51 +0700

    login page

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,13 @@
+{{-- <div class="panel panel-default">
+    <div class="panel-body">
+        Panel Content
+    </div>
+    <div class="panel-footer">Panel footer</div>
+</div> --}}
+
+<div class="card">
+  <div class="card-body">
+    card content
+  </div>
+  <div class="card-footer">card footer</div>
+</div>
