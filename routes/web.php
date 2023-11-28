f2cb476521d988412e72169355e1242af3a77e31
Ridho Aulia Rahman <220605110044@student.uin-malang.ac.id>
2023-11-28 10:54:37 +0700

    login udah haha

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\UserAuth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::middleware(['web', UserAuth::class])->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+});
+Route::get('/login', function () {
     return view('login');
 });
+
+Route::post("/login", [UserController::class, 'login']);
